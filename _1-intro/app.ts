36c791917c61e6e0da32f80948fa1adcde9e369e
Thiago Vieira <th91v@outlook.com>
2020-03-15 19:56:17 -0300

    literal types

--- _1-intro/app.ts
+++ _1-intro/app.ts
@@ -1,16 +1,32 @@
 // (type1 | type2 | type3 | .. | typeN)
-function combine(input1: number | string, input2: number | string){
+function combine(
+    input1: number | string,
+    input2: number | string,
+    resultConversion: 'as-number' | 'as-text'){ // sintaxe de Literal Type
+
     let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number'){
-        result = input1 + input2;
+
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString();
     }
+
     return result;
+
+    // if (resultConversion === 'as-number') {
+    //     return +result; // força conversão de result para número
+    // } else {
+    //     return result.toString() // força conversão de result para string
+    // }
 }
 
-const combinedAges = combine(30, 26);
-const combinedNames = combine('Max', 'Anna');
+const combinedAges = combine(30, 26, 'as-number');
+
+const combinedStringAges = combine('30', '26', 'as-number');
+
+const combinedNames = combine('Max', 'Anna', 'as-text');
 
 console.log(combinedAges);
 console.log(combinedNames);
+console.log(combinedStringAges);
