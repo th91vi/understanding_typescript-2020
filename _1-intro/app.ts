a51b08f5aa999d0ef047bd6977aa65da1629e490
Thiago Vieira <th91v@outlook.com>
2020-03-15 20:44:30 -0300

    function types, callback functions

--- _1-intro/app.ts
+++ _1-intro/app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
     console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, callback: (num: number) => void){
+    const result = n1 + n2;
+    callback(result);
+}
+
 printResult(add(5, 12)); // Result: 17
 console.log(printResult(add(5, 12))); // undefined
 
@@ -19,3 +24,7 @@ combineValues = add;
 console.log(combineValues(8,8));
 
 // let someValue: undefined;
+
+addAndHandle(10, 20, (output) => {
+    console.log(output)
+})
