7e64d44c5cebdef584823fb838a07d746b83e06d
Thiago Vieira <th91v@outlook.com>
2020-03-15 19:19:21 -0300

    union types

---
+++ _1-intro/app.ts
@@ -0,0 +1,16 @@
+// (type1 | type2 | type3 | .. | typeN)
+function combine(input1: number | string, input2: number | string){
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number'){
+        result = input1 + input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+
+const combinedAges = combine(30, 26);
+const combinedNames = combine('Max', 'Anna');
+
+console.log(combinedAges);
+console.log(combinedNames);
