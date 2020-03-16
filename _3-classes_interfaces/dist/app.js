ea3e41ebb4bee19e503fd21e8f77cfeffbc7355f
Thiago Vieira <th91v@outlook.com>
2020-03-16 19:20:52 -0300

    classes e construtores, this

---
+++ _3-classes_interfaces/dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+var Department = /** @class */ (function () {
+    function Department(n) {
+        this.name = n;
+    }
+    Department.prototype.describe = function () {
+        console.log('Department: ' + this.name);
+    };
+    return Department;
+}());
+var accounting = new Department('Accounting');
+accounting.describe();
+// const accountingCopy = { describe: accounting.describe };
+var accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+accountingCopy.describe();
---
+++ _3-classes_interfaces/src/app.ts
@@ -0,0 +1,20 @@
+class Department {
+    name: string;
+
+    constructor(n: string){
+        this.name = n;
+    }
+
+    describe(this: Department) {
+        console.log('Department: ' + this.name);
+    }
+}
+
+const accounting = new Department('Accounting');
+
+accounting.describe();
+
+// const accountingCopy = { describe: accounting.describe };
+const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
+accountingCopy.describe();
