b4a2b7a3c43954f42ea7d2a70ff5c8d768cca1d7
Thiago Vieira <th91v@outlook.com>
2020-03-15 21:18:43 -0300

    never type

--- _1-intro/app.ts
+++ _1-intro/app.ts
@@ -8,4 +8,10 @@ if (typeof userInput === 'string') {
     userName = userInput;
 }
 
+function generateError(message: string, code: number): never {
+    throw { message: message, errorCode: code};
+    // while (true);
+}
+
 console.log(userName);
+generateError('An error occured: ', 500)
