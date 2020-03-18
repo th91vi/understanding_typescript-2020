110a19836703efe1f0b2e3fd41136d2ad0931948
Thiago Vieira <th91v@outlook.com>
2020-03-17 22:01:59 -0300

    classes with interfaces

--- _3-classes_interfaces/dist/app.js
+++ _3-classes_interfaces/dist/app.js
@@ -1,10 +1,14 @@
 "use strict";
-let user1;
-user1 = {
-    name: 'Max',
-    age: 30,
+class Person {
+    constructor(n) {
+        this.age = 30;
+        this.name = n;
+    }
     greet(phrase) {
         console.log(`${phrase}! My name is ${this.name}`);
     }
-};
+}
+let user1;
+user1 = new Person('Thiago');
 user1.greet('Hello');
+console.log(user1);
--- _3-classes_interfaces/src/app.ts
+++ _3-classes_interfaces/src/app.ts
@@ -1,18 +1,24 @@
-interface Person{
+interface Greetable {
     name: string;
-    age: number;
-
     greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+    name: string;
+    age = 30;
 
-user1 = {
-    name: 'Max',
-    age: 30,
+    constructor(n: string){
+        this.name = n;
+    }
+
     greet(phrase: string){
         console.log(`${phrase}! My name is ${this.name}`);
     }
 }
 
+let user1: Greetable;
+
+user1 = new Person('Thiago');
+
 user1.greet('Hello');
+console.log(user1);
