2876539c821fd707d3336279d62766b19596e38e
Thiago Vieira <th91v@outlook.com>
2020-04-20 07:53:59 -0300

    more on type guards

--- _4-advanced_types/dist/app.js
+++ _4-advanced_types/dist/app.js
@@ -1,6 +1,45 @@
 "use strict";
 const e1 = {
     name: 'Max',
-    privilegies: ['create-server'],
+    privileges: ['create-server'],
     startDate: new Date()
 };
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Privileges: ' + emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+class Car {
+    drive() {
+        console.log('Driving a car...');
+    }
+}
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+    loadCargo(amount) {
+        console.log('Loading cargo... ' + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if ('loadCargo' in vehicle) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- _4-advanced_types/src/app.ts
+++ _4-advanced_types/src/app.ts
@@ -1,6 +1,6 @@
 type Admin = {
     name: string;
-    privilegies: string[];
+    privileges: string[];
 };
 
 type Employee = {
@@ -14,7 +14,7 @@ type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
     name: 'Max',
-    privilegies: ['create-server'],
+    privileges: ['create-server'],
     startDate: new Date()
 }
 
@@ -22,3 +22,56 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: number, b: number){
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+
+    return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee){
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp){
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Privileges: ' + emp.startDate);
+    }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+    drive() {
+        console.log('Driving a car...');
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+
+    loadCargo(amount: number) {
+        console.log('Loading cargo... ' + amount);
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+    if ('loadCargo' in vehicle) {
+        vehicle.loadCargo(1000);
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
